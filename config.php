e199ba1b04af17d7eafe64aaed0b69af7e7195c7
Ilya Korolev <ilya.korolev@teligent.ru>
2012-08-27 11:00:13 +0400

    ...

--- config.php
+++ config.php
@@ -8,12 +8,9 @@
 	$templatedir    = "templates";
 	$collectionconf = "$rootdir/plugins/$defaultdir/collection.conf";
 
-	$colors = array("00AA00", "0000FF", "FF0000", "ffb000",
+	$colors = array("00AA00", "0000FF", "FF0000",
 				  "ff00ff",
-				  "ff0000",
 				  "00e000",
-				  "0000ff",
-				  "ff00ff",
 				  "a000a0",
 				  "ffb000",
 				  "ff8000",
